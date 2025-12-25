75b7371c0127cf4a1129a2ab05befe650a57ffb2
usama azam <usamaazam5654@gmail.com>
2025-12-25 10:39:35 +0500

    Adding pages and creating the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignOut from "./Pages/SignOut";
+import Profile from "./Pages/Profile";
 
-
-function App() {
- 
-
+const App=()=> {
   return (
     <>
-      frontend
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Profile" element={<Profile/>} />
+          <Route path="/SignIn" element={<SignIn/>} />
+          <Route path="/SignOut" element={<SignOut/>} />
+          
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
